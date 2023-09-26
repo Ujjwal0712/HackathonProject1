bfdff8e50cc8a9729b99005f64a59dc93ddd445c
Ujjwal0712 <119096131+Ujjwal0712@users.noreply.github.com>
2023-09-27 03:26:42 +0530

    Login and CreateAccount

--- app/src/main/java/com/example/prog_o_thonproject/AmbassadorHomeActivity.kt
+++ app/src/main/java/com/example/prog_o_thonproject/AmbassadorHomeActivity.kt
@@ -3,9 +3,9 @@ package com.example.prog_o_thonproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AmbassadorHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_ambassador_home)
     }
 }
---
+++ app/src/main/java/com/example/prog_o_thonproject/LoginActivity.kt
@@ -0,0 +1,55 @@
+package com.example.prog_o_thonproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.prog_o_thonproject.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var auth: FirebaseAuth
+    private val duration = Toast.LENGTH_SHORT
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth= FirebaseAuth.getInstance()
+        binding.login.setOnClickListener{
+            loginuser()
+        }
+    }
+
+    private fun loginuser() {
+        val email = binding.username.text.toString()
+        val password = binding.password.text.toString()
+
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+
+
+                if (task.isSuccessful && email.contains("campusambassador", ignoreCase = true)) {
+                    val intent = Intent(this, AmbassadorHomeActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    // Login success
+                    auth.currentUser
+                }else if (task.isSuccessful){
+                    val intent= Intent(this,StudentHomeActivity::class.java)
+                    startActivity(intent)
+                }
+
+                else
+                {
+                    val toast = Toast.makeText(this@LoginActivity, "Login Failed",duration)
+                    toast.show()
+                    // Login failed
+                }
+            }
+        }
+    }
+}
